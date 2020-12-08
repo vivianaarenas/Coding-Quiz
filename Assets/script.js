dde39b4722adf299f26181599fc2e56001673f42
Viviana <vivianaaarenas@gmail.com>
2020-12-08 15:00:55 -0500

    Add start button,

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,22 @@ var pos = 0;
 var correct = 0;
 var test, test_status, question, choice, choices, chA, chB, chC;
 
+var test = document.getElementById("test");
+
+const startBtn = document.getElementById("startButton");
+
+startButton.addEventListener("click", startBtn);
+
+startButton.addEventListener("click", questions);
+// startButton.addEventListener("click", startTimer);
+startButton.addEventListener("click", () => {
+  messageDiv.textContent = "";
+});
+chA.addEventListener("click", choice0);
+chB.addEventListener("click", choice1);
+chC.addEventListener("click", choice2);
+
+startQuiz.onclick = startTimer;
 //array of questions from which the code quiz pulls from, it's an multidimensional array with 4 inner array elements
 var questions = [
   {
@@ -42,3 +58,28 @@ var questions = [
 function get(x) {
   return document.getElementById(x);
 }
+
+function renderQuestion() {
+  test = get("test");
+  if (pos >= questions.length) {
+    test.innerHTML =
+      "<h2>You got " +
+      correct +
+      "of" +
+      questions.length +
+      "questions correct</h2>";
+    get("test_status").innerHTML = "Test completed"; // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+}
+
+get("test_status").innerHTML =
+  "Question " + (pos + 1) + " of " + questions.length;
+
+question = questions[pos].question;
+chA = questions[pos].a;
+chB = questions[pos].b;
+chC = questions[pos].c;
